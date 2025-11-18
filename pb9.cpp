7d2d33d79dad11e85e6c18e5df20bd2e59231df1
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-11-18 23:05:40 +0530

    Ninth Pattern

--- pb9.cpp
+++ pb9.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+void pb_7(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+           cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
 void pb_8(int n){
     for(int i=0;i<n;i++){
         for(int j=1;j<=i;j++){
@@ -15,6 +26,7 @@ int main(){
     int n;
     cout<<"Enter the number n";
     cin>>n;
+    pb_7(n);
     pb_8(n);
     return 0;
 }
