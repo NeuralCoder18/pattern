65de2444d356f69564c701d9dd45beb12fe0b678
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-11-19 16:45:24 +0530

    Thirteenth Pattern

---
+++ pb13.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void pb13(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num++;
+        }
+       
+        cout<<endl;
+        
+    }
+        
+    }
+
+int main(){ 
+    int n;
+    cout<<"Enter the number n";
+    cin>>n;
+    pb13(n);
+    return 0;
+}
