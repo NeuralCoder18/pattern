d4f5c529a87e082cdbba950829bbf5b153b82f03
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-11-20 21:09:45 +0530

    Sixteenth Pattern

---
+++ pb16.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void pb16(int n){
+    char ch='A';
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++){
+            cout<<ch<<" ";
+            
+        }
+        ch=ch+1;
+        cout<<endl;
+        
+    }
+        
+    }
+
+int main(){ 
+    int n;
+    cout<<"Enter the number n";
+    cin>>n;
+    pb16(n);
+    return 0;
+}
